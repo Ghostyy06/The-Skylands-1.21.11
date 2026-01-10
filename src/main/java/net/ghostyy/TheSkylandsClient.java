ab05b77f99f50cee7702707f7f828f94fc6ea0bc
jameson <jkoproski06@gmail.com>
2026-01-10 14:05:46 -0500

    test1

---
+++ src/main/java/net/ghostyy/TheSkylandsClient.java
@@ -0,0 +1,10 @@
+package net.ghostyy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheSkylandsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
